38a8f474ebcb0cd84d4b50e89c300f0da8c5f159
Enea Zguro <eneazguro02@gmail.com>
2026-04-14 14:56:41 -0400

    dotenv update

--- public/vepa-booking-server/booking-server-index.js
+++ public/vepa-booking-server/booking-server-index.js
@@ -1,4 +1,6 @@
-require('dotenv').config(); // Assuming you're using dotenv locally
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const { google } = require('googleapis');
